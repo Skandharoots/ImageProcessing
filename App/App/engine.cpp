a9f255243ac15fdf5483b11b7713498fb7c3b229
Marek Kopania <m.k.kopania@gmail.com>
2023-10-03 17:56:30 +0200

    initial updates to the application

--- App/App/engine.cpp
+++ App/App/engine.cpp
@@ -2,6 +2,11 @@
 // Created by skand on 03/10/2023.
 //
 
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <regex>
 #include "engine.h"
 #include "CImg.h"
 
@@ -9,14 +14,71 @@
 using namespace cimg_library;
 
 Engine::Engine() {
-
+    
 }
 
 Engine::~Engine() {
 
 }
 
+void Engine::setCommand(std::string command) {
+    this->command = command;
+}
+
+std::string Engine::getCommand() {
+    return this->command;
+}
+
+void Engine::setName(std::string name) {
+    this->name = name;
+}
+
+std::string Engine::getName() {
+    return this->name;
+}
+
+void Engine::setParams(std::string params) {
+    this->params = params;
+}
+
+std::string Engine::getParams() {
+    return this->params;
+}
+
 void Engine::openImage() {
+    std::string command;
+    const std::regex txt_regex("[a-z]+[ ][-][-][a-z]+[ ][0-9]+");
+    int i = 0;
+    bool format = false;
+    while (format == false) {
+        std::cout << "Hello, please provide the parameters according to the format below : \n" << "name --command [-argument=value [...]] \n";
+        std::getline(std::cin, command);
+        if (std::regex_match(command, txt_regex) != 1) {
+            format = false;
+        }
+        else {
+            format = true;
+        }
+    }
+    std::istringstream ss(command);
+    std::string del;
+    while (getline(ss, del, ' ')) {
+        if (i == 0) {
+            this->name = del;
+            i++;
+        } 
+        if (i == 1) {
+            this->command = del;
+            i++;
+        }
+        if (i == 2) {
+            this->params = del;
+        }
+        else {
+            std::cout << "Wrong number of parameters!\n";
+        }
+    }
+    std::cout << this->name << ", " << this->command << ", " << this->params << "\n";
     CImg<unsigned char> image("../images/lenac.bmp"); // create the image from a file (must exist in the working dir)
     for (int x = 0; x < image.width(); x++) {
         for (int y = 0; y < image.height() / 2; y++) { // only upper half of the image gets processed
--- App/App/engine.h
+++ App/App/engine.h
@@ -1,7 +1,20 @@
 #pragma once
+
+#include <string>
+
 class Engine {
+private:
+    std::string command;
+    std::string name;
+    std::string params;
 public:
     Engine();
     ~Engine();
+    void setCommand(std::string command);
+    std::string getCommand();
+    void setName(std::string name);
+    std::string getName();
+    void setParams(std::string params);
+    std::string getParams();
     void openImage();
 };
