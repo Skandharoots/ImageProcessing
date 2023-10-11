ca82085972a57485502e4f916ef9227d8d01ed4b
Marek Kopania <m.k.kopania@gmail.com>
2023-10-11 22:09:51 +0200

    Cleaned repo

--- Application_1/build/program/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/skand/Documents/University/5sem/ImageProcessing/ImageProcessing/Application_1/program
-# Build directory: C:/Users/skand/Documents/University/5sem/ImageProcessing/ImageProcessing/Application_1/build/program
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
