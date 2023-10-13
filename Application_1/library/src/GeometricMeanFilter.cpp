544477eec52e258c2d443259b96827a1db96c01e
Marek Kopania <m.k.kopania@gmail.com>
2023-10-13 21:48:59 +0200

    Cleanup

--- Application_1/library/src/GeometricMeanFilter.cpp
+++ Application_1/library/src/GeometricMeanFilter.cpp
@@ -36,27 +36,16 @@ void GeometricMeanFilter::filter() {
 	try {
         CImg<unsigned char> image(getInputPath().c_str());
         CImg<unsigned char> copy(getInputPath().c_str());
+        float std_dev = 20;
         float sum0 = 1;
         float sum1 = 1;
         float sum2 = 1;
-        float mean0 = 0;
-        float mean1 = 0;
-        float mean2 = 0;
-        for (int x = 0; x < image.width(); x++) {
-            for (int y = 1; y < image.height(); y++) {
-                mean0 += image(x, y, 0);
-                mean1 += image(x, y, 1);
-                mean2 += image(x, y, 2);
-            }
-        }
-        mean0 = mean0 / (image.width() * image.height());
-        mean1 = mean1 / (image.width() * image.height());
-        mean2 = mean2 / (image.width() * image.height());
         for (int x = 1; x < copy.width() - 1; x++) {
                 for (int y = 1; y < copy.height() - 1; y++) {
                     if (((copy(x, y, 0) == 0) && (copy(x, y, 1) == 0) && (copy(x, y, 2) == 0)) || 
-                        ((copy(x, y, 0) == 255) && (copy(x, y, 1) == 255) && (copy(x, y, 2) == 255)) ||
-                        ((mean0 - copy(x, y, 0) > 20) || (mean1 - copy(x, y, 1) > 20) || (mean2 - copy(x, y, 2) > 20))) {
+                        ((copy(x, y, 0) == 255) && (copy(x, y, 1) == 255) && (copy(x, y, 2) == 255))
+                        
+                        ) {
                         for (int i = x - 1; i < x + 2; i++) { //loop for the first x in box of 9
                             for (int j = y - 1; j < y + 2; j++) { // loop for the first y in box of 9
                                 if ((i == x) && (j == y)) {
@@ -73,7 +62,8 @@ void GeometricMeanFilter::filter() {
                         image(x, y, 0) = pow(sum0, 0.111111111);
                         image(x, y, 1) = pow(sum1, 0.111111111);
                         image(x, y, 2) = pow(sum2, 0.111111111);
-                    } 
+                    }
+                     
                     sum0 = 1;
                     sum1 = 1;
                     sum2 = 1;
