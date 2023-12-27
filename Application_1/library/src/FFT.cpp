c16aac9205b8d10d47e6b708035ec5f9e8f25cb7
Marek Kopania <234760@edu.p.lodz.pl>
2023-12-27 23:01:19 +0100

    update: potentially fixed fft

--- Application_1/library/src/FFT.cpp
+++ Application_1/library/src/FFT.cpp
@@ -42,6 +42,7 @@ void FFT::transform() {
         i = sqrt(i);
 		CImg<unsigned char> image(getInputPath().c_str());
 		CImg<unsigned char> image2(getInputPath().c_str());
+        CImg<unsigned char> image3(getInputPath().c_str());
         double real = 0;
         double imaginary = 0;
         double result = 0;
@@ -56,28 +57,71 @@ void FFT::transform() {
             }
         }
 
-        
-        for (int x = 0; x < image.width(); x++) {
-            for (int y = 0; y < image.height(); y++) {
+        for (int y = 0; y < image.height(); y++) {
+            for (int x = 0; x < image.width(); x++) {
+                // for (int xx = 0; xx < image.width(); xx++) {
+                //     real += image(xx, y, 0) * cos((2.0 * 3.1415926 * x * xx) / image.height());
+                //     imaginary += image(xx, y, 0) * -1 * sin((2.0 * 3.1415926 * x * xx) / image.height());
+                // }
                 for (int yy = 0; yy < image.height(); yy++) {
                     real += image(x, yy, 0) * cos((2.0 * 3.1415926 * y * yy) / image.height());
                     imaginary += image(x, yy, 0) * -1 * sin((2.0 * 3.1415926 * y * yy) / image.height());
-                }
-                for (int xx = 0; xx < image.width(); xx++) {
-                    real += image(xx, y, 0) * cos((2.0 * 3.1415926 * x * xx) / image.height());
-                    imaginary += image(xx, y, 0) * -1 *sin((2.0 * 3.1415926 * x * xx) / image.height());
                 }
                 result = sqrt(pow(real, 2) + pow(imaginary, 2));
                 image2(x, y, 0) = result;
                 image2(x, y, 1) = result;
                 image2(x, y, 2) = result;
-
                 real = 0;
                 imaginary = 0;
                 result = 0;
-            } 
+
+            }
         }
-		image2.save_bmp(getOutputPath().c_str());
+        real = 0;
+        imaginary = 0;
+        result = 0;
+        for (int x = 0; x < image2.width(); x++) {
+            for (int y = 0; y < image2.height(); y++) {
+                // for (int yy = 0; yy < image.height(); yy++) {
+                //     real += image2(x, yy, 0) * cos((2.0 * 3.1415926 * y * yy) / image2.height());
+                //     imaginary += image2(x, yy, 0) * -1 * sin((2.0 * 3.1415926 * y * yy) / image2.height());
+                // }
+                for (int xx = 0; xx < image2.width(); xx++) {
+                    real += image2(xx, y, 0) * cos((2.0 * 3.1415926 * x * xx) / image2.width());
+                    imaginary += image2(xx, y, 0) * -1 * sin((2.0 * 3.1415926 * x * xx) / image2.width());
+                }
+                result = sqrt(pow(real, 2) + pow(imaginary, 2));
+                image3(x, y, 0) = result;
+                image3(x, y, 1) = result;
+                image3(x, y, 2) = result;
+                real = 0;
+                imaginary = 0;
+                result = 0;
+            }
+        }
+
+        
+        // for (int x = 0; x < image.width(); x++) {
+        //     for (int y = 0; y < image.height(); y++) {
+        //         for (int yy = 0; yy < image.height(); yy++) {
+        //             real += image(x, yy, 0) * cos((2.0 * 3.1415926 * y * yy) / image.height());
+        //             imaginary += image(x, yy, 0) * -1 * sin((2.0 * 3.1415926 * y * yy) / image.height());
+        //         }
+        //         for (int xx = 0; xx < image.width(); xx++) {
+        //             real += image(xx, y, 0) * cos((2.0 * 3.1415926 * x * xx) / image.height());
+        //             imaginary += image(xx, y, 0) * -1 *sin((2.0 * 3.1415926 * x * xx) / image.height());
+        //         }
+        //         result = sqrt(pow(real, 2) + pow(imaginary, 2));
+        //         image2(x, y, 0) = result;
+        //         image2(x, y, 1) = result;
+        //         image2(x, y, 2) = result;
+
+        //         real = 0;
+        //         imaginary = 0;
+        //         result = 0;
+        //     } 
+        // }
+		image3.save_bmp(getOutputPath().c_str());
 	}
 	catch (CImgIOException e) {
 		throw std::exception("Cannot load or save from the path. Path invalid.\n");
